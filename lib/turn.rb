90f0a651d4fab9f76453a8e9f9c1317b143d8136
Elle Faraday <faraday.elle@gmail.com>
2017-10-15 02:30:04 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,28 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(input)
+  index = input - 1
+end
+
+def position_taken?(board, index)
+  if board[index] == "" || board[index] == " " || board[index] == nil
+    return false
+  else
+    return true
+  end
+end
+
+def valid_move?(board, index)
+  if index <= 8 && index >= 0 && position_taken?(board,index) == false
+    return true
+  else
+    return false
+  end
+end
+
